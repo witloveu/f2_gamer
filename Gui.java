87fc1ab572abd6582f5f565364683dc7b8bc5f2c
witloveu <immortal.112@hotmail.com>
2016-04-21 00:08:16 +0700

    #01 Making GUI
    1.สร้างหน้าจอสีดำ ( GUI ) ขึ้นมา

---
+++ Gui.java
@@ -0,0 +1,17 @@
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class Gui {
+	public static void main(String[] args){
+		JFrame frame = new JFrame("Spaceship");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500,700);
+
+		frame.getContentPane().setBackground(Color.BLACK);
+		frame.setVisible(true);
+		
+	
+		
+		
+	}
+}
