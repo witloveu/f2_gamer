d9f554c6cc7e31e735f1b5a714acff8f1293a2cb
witloveu <immortal.112@hotmail.com>
2016-04-30 22:52:57 +0700

    #08 Die when crashed ( ตายเมื่อชนสิ่งกีดขวาง )

--- GameEngine.java
+++ GameEngine.java
@@ -71,6 +71,16 @@ public class GameEngine implements KeyListener{
 			}
 		}
 		gp.updateGameUI(this);	
+
+		Rectangle2D.Double vr = v.getRectangle();
+		Rectangle2D.Double er;
+		for(Enemy e : enemies){
+			er = e.getRectangle();
+			if(er.intersects(vr)){
+				die();
+				
+			}
+		}
 		
 	}
 	
